3536cf45cfeded51a42f59f99cdbfc069f056559
zahidul islam <zahidulislam5594@gmail.com>
2019-12-19 22:02:41 +0600

    add next and previous method use paramMap

--- src/app/RoutParameter/WorkDetails.component.ts
+++ src/app/RoutParameter/WorkDetails.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
     selector:'app-workDetails',
@@ -7,14 +7,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WorkDetailsComponent implements OnInit{
     public workerId;
-    constructor(private route:ActivatedRoute) {
+    constructor(private route:ActivatedRoute,private router:Router) {
         
     }
     ngOnInit(): void {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         //Add 'implements OnInit' to the class.
-   let id=parseInt(this.route.snapshot.paramMap.get('id'));    
-   this.workerId=id;
+
+        // let id=parseInt(this.route.snapshot.paramMap.get('id'));    
+        // this.workerId=id;
+        this.route.paramMap.subscribe((param:ParamMap)=>{
+            let id=parseInt(param.get('id'));
+            this.workerId=id;
+        });
+    }
+    goPrevious(){
+        let previousId=this.workerId-1;
+        this.router.navigate(['/worker',previousId]);
+    }
+
+    goNext(){
+        let previousId=this.workerId+1;
+        this.router.navigate(['/worker',previousId]);
     }
 
 }
